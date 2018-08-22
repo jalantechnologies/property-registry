10ee61ad68360dafd79b91e5235c9f3a114b1bba
Ankit Bhawnani <bbbneo333@gmail.com>
2018-08-22 18:37:57 +0530

    webpack.config.js - public realm is now optional

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const webpack = require('webpack');
 const config = require('config');
 
+const CONFIG = config.has('public') ? config.get('public') : {};
+
 module.exports = {
   plugins: [
     new webpack.DefinePlugin({
-      CONFIG: JSON.stringify(config.get('public')),
+      CONFIG,
     }),
   ],
 };
