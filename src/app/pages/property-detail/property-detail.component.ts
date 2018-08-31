4d470f05f0bf184db6ab84db74b926589bf3a947
Sakshi Gokhru <sakshi@jalantechnologies.com>
2018-08-31 16:07:42 +0530

    Added deed url and added property ownership histore feature

--- src/app/pages/property-detail/property-detail.component.ts
+++ src/app/pages/property-detail/property-detail.component.ts
@@ -14,21 +14,36 @@ export class PropertyDetailComponent implements OnInit {
   propertyDetail: any;
   tokenContractAddress = '0x2e44570a4cbfedb5372ea39a907fc814b1692be6';
   propertyDetailError: any;
+  loadingDeedHistory = false;
 
   constructor(private route: ActivatedRoute, private contractService: ContractsService) {}
 
   ngOnInit() {
     this.propertyAddress = this.route.snapshot.paramMap.get('propertyAddress');
     if (this.propertyAddress) {
-      this.contractService.getPropertyOwnerDetails(this.tokenContractAddress, this.propertyAddress, 1).then(response => {
-        this.propertyDetail = {
-          ownerName: response[0],
-          ownerEmail: response[1],
-          ownerWalletAddress: response[2]
-        };
-      }).catch(err => {
-        this.propertyDetailError = true;
-      });
+      this.getDeedHistory(this.propertyAddress, 1);
     }
+  }
+
+  getDeedHistory(propertyAddress, index) {
+    this.propertyDetail = {};
+    this.loadingDeedHistory = true;
+    this.contractService.getPropertyOwnerDetails(this.tokenContractAddress, this.propertyAddress, index).then(response => {
+      this.propertyDetail = {
+        ownerName: response[0],
+        ownerEmail: response[1],
+        ownerWalletAddress: response[2],
+        deedURL: response[3],
+        nextIndexExist: response[4],
+        previousIndexExist: response[5],
+        propertyAddress: propertyAddress,
+        index: index
+      };
+      this.loadingDeedHistory = false;
+    }).catch(err => {
+      this.loadingDeedHistory = false;
+      this.propertyDetailError = true;
+      this.loadingDeedHistory = false;
+    });
   }
 }
