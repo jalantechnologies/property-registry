c895bec44c912a765ed6bd785788766717109c18
Sakshi Gokhru <sakshi@jalantechnologies.com>
2018-09-11 16:18:35 +0530

    Improve changes of env variables

--- config/env.js
+++ config/env.js
@@ -2,6 +2,7 @@ module.exports = {
   public: {
     ropstenURL: process.env.ropstenURL,
     contractAddress: process.env.contractAddress,
+    apiEndPoint: process.env.apiEndPoint,
   },
   www: {
     port: process.env.PORT,
