fbc5555ce37a79f511041e215cf4a3c3fb23246c
sakshigokhru <38902768+sakshigokhru@users.noreply.github.com>
2018-09-12 10:58:07 +0530

    PROP-3 : Store property address in mongoDB (#2)
    * On property creation event trigger, stored property address in mongodb
    
    * Improve changes
    
    * Improve changes of env variables
    
    * Improve changes

--- config/env.js
+++ config/env.js
@@ -2,6 +2,7 @@ module.exports = {
   public: {
     ropstenURL: process.env.ropstenURL,
     contractAddress: process.env.contractAddress,
+    apiEndpoint: process.env.apiEndpoint,
   },
   www: {
     port: process.env.PORT,
